c7052f4c474c40bf8f5e0a51560962dffb3d0fcf
deathcoi <deathcoi1711@gmail.com>
2021-09-21 03:19:26 +0700

    first time ( created )

---
+++ app/src/main/java/com/example/sharksupplement/TDEE.java
@@ -0,0 +1,13 @@
+package com.example.sharksupplement;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TDEE extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tdee);
+    }
+}
